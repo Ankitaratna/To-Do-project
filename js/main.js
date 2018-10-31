04193920ba4b55678935e2de44b9219b4668463b
Shivam <shivampkumar@gmail.com>
2018-10-31 19:02:15 +0530

    change colour and allignments

--- js/main.js
+++ js/main.js
@@ -3,6 +3,10 @@ function tolist()
 	var item= document.getElementById("doinput").value
 	var text= document.createTextNode(item)
 	var intem= document.createElement("LI")
+	
+	
+
 	intem.appendChild(text)
+	intem.appendChild(button)
 	document.getElementById("listem").appendChild(intem)
 }
